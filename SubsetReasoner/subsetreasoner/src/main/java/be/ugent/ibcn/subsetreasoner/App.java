86a1f2c52ee08d32dcdda303181538a555ce45f6
pbonte <pieter.bonte@intec.ugent.be>
2017-05-30 09:45:03 +0200

    Initial commit

---
+++ SubsetReasoner/subsetreasoner/src/main/java/be/ugent/ibcn/subsetreasoner/App.java
@@ -0,0 +1,13 @@
+package be.ugent.ibcn.subsetreasoner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
